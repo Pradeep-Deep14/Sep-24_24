1b2768dfd0e30355e7dc01cd93dced30209c97f5
Pradeep <deeppradeep1998@gmail.com>
2024-09-24 20:34:17 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+socialMedias=set()
+socialMedias.add("Instagram")
+socialMedias.add("Snapchat")
+
+socialMedias.add("Facebook")
+socialMedias.add("Facebook")
+socialMedias.remove("Facebook")
+
+
+print(len(socialMedias))
